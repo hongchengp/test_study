ed87268318fecb1f038fbe70aa05140b42fe8584
hcp <2560899500@qq.com>
2024-11-11 21:52:19 +0800

    对于相同的t.Fatalf, 进行了重构

--- go_base/pointers/v2/wallet_test.go
+++ go_base/pointers/v2/wallet_test.go
@@ -3,25 +3,26 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
+
+	assertBlance := func(t *testing.T, w *Wallet, want Bitcoin) {
+		got := w.Balance()
+		if want != got {
+			t.Fatalf("want %s, got %s", want, got)
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
-		got := wallet.Balance()
 		want := Bitcoin(10)
-		if got != want {
-			t.Fatalf("want %s, got %s", want, got)
-		}
+		assertBlance(t, &wallet, want)
 	})
 
 	t.Run("WithDraw", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(20)
 		wallet.WithDraw(10)
-		got := wallet.Balance()
 		want := Bitcoin(10)
-		if got != want {
-			t.Fatalf("want %s, got %s", want, got)
-		}
+		assertBlance(t, &wallet, want)
 	})
 }
 
