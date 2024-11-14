6c6ed9b295e496a4f70f62101c7d4453beffbe11
hcp <2560899500@qq.com>
2024-11-14 13:15:59 +0800

    如果字段是结构体，递归调用

---
+++ go_base/refelct/v3/v2/walk.go
@@ -0,0 +1,15 @@
+package main
+
+import "reflect"
+
+func walk(x interface{}, fn func(string)) {
+	val := reflect.ValueOf(x)
+	for i := range val.NumField() {
+		v := val.Field(i)
+		if v.Kind() == reflect.String {
+			fn(v.String())
+		} else if v.Kind() == reflect.Struct {
+			walk(v.Interface(), fn)
+		}
+	}
+}
